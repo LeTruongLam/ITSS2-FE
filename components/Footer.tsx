40eb442cd2ea8ca133f89553fa057cbe58bac7e8
xd3011 <xuandat3011@gmail.com>
2024-01-11 20:15:33 +0700

    Fix HomePage 01

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import Link from "next/link";
 import Image from "next/image";
 import LogoPurple from "../public/logo-purple.svg";
-import LogoFaceBook from "../public/logo-facebook.svg";
+import LogoGmail from "../public/logo-gmail.svg";
 import LogoGitHub from "../public/logo-github.svg";
 import { footerLinks } from "@/constants";
 type Link = { link: string; icon: string };
@@ -13,12 +13,12 @@ type ColumnProps = {
 
 const FooterColumn = ({ title, links }: ColumnProps) => (
   <div className="footer_column">
-    <h4 className="font-semibold">{title}</h4>
+    <h4 className="font-semibold text-slate-200 text-xl">{title}</h4>
     <div style={{ display: 'flex', gap: "10px" }}>
       {links.map((e) => (
-        <Link href={e.link}>
-          <Image src={e.icon === "LogoFaceBook" ? LogoFaceBook : LogoGitHub} width={32} height={32} alt={e.icon} />
-        </Link>
+        <a href={e.link.includes("@gmail.com") ? "mailto:e.link" : e.link}>
+          <Image src={e.icon === "LogoGmail" ? LogoGmail : LogoGitHub} width={32} height={32} alt={e.icon} />
+        </a>
       ))}
     </div>
   </div >
@@ -26,11 +26,11 @@ const FooterColumn = ({ title, links }: ColumnProps) => (
 const Footer = () => {
   return (
     <footer className="flexStart footer">
-      <div className="flex flex-col gap-12 w-full">
-        <div className="flex items-start flex-col">
+      <div className="flex flex-col gap-12 w-full ">
+        <div className="flex items-start flex-col text-slate-200">
           <Image src={LogoPurple} width={115} height={38} alt="Flexibble" />
           <p className="text-start text-sm font-normal mt-5 max-w-xs">
-            Gioi thieu nhom.
+            Gioi thieu nhom bla bla....
           </p>
         </div>
         <div className="flex flex-wrap gap-12">
@@ -41,12 +41,12 @@ const Footer = () => {
           ))}
         </div>
       </div>
-      <div className="flexBetween footer_copyright">
+      <div className="flexBetween footer_copyright text-slate-200">
         <p>@ 2023 Flexibble. All rights reserved</p>
-        <p className="text-gray">
+        {/* <p className="text-gray">
           <span className="text-black font-semibold">10,214</span> projects
           submitted
-        </p>
+        </p> */}
       </div>
     </footer>
   );
