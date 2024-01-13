4cf4fcf0d8679defc1d244dc7eef064e03c0d032
LeTruongLam <104148284+LeTruongLam@users.noreply.github.com>
2024-01-13 22:38:31 +0700

    fix: add cards

--- app/decks/[id]/edit-card/page.tsx
+++ app/decks/[id]/edit-card/page.tsx
@@ -1,73 +1,92 @@
-"use client"
+"use client";
 import React, { useState, useEffect } from "react";
-import { Input, Button } from "antd";
+import { Input, Select, Button } from "antd";
+
+const { Option } = Select;
 
 const CreateDeck = () => {
   const url = window.location.href;
 
-  const [deckId, setDeckId] = useState("");
+  const [deckId, setDeckId] = useState();
   const [cards, setCards] = useState([]);
-
+  const [frontCard, setFrontCard] = useState("");
+  const token =
+    typeof window !== "undefined" ? localStorage.getItem("token") : null;
   useEffect(() => {
     const id = url.split("/")[4];
-    setDeckId(id);
-  }, [url]);
 
+    setDeckId(id);
+  }, []);
   const handleAddCard = () => {
-    setCards((prevCards) => [
-      ...prevCards,
-      {
-        front: "",
-        back: {
-          definition: "",
-          synonyms: "",
-          example: "",
-        },
-        deck_id: deckId,
-      },
+    setCards([
+      ...cards,
+      { front: "", back: { definition: "", synonyms: "", example: "" } },
     ]);
   };
 
   const handleCardChange = (index, field, value) => {
-    setCards((prevCards) => {
-      const updatedCards = [...prevCards];
-      const updatedCard = {
-        ...updatedCards[index],
-        back: {
-          ...updatedCards[index].back,
-          [field]: value,
-        },
-      };
-      updatedCards[index] = updatedCard;
-      return updatedCards;
-    });
+    const updatedCards = [...cards];
+    updatedCards[index] = {
+      ...updatedCards[index],
+      [field]: value,
+    };
+    setCards(updatedCards);
   };
 
   const handleRemoveCard = (index) => {
-    setCards((prevCards) => {
-      const updatedCards = [...prevCards];
-      updatedCards.splice(index, 1);
-      return updatedCards;
-    });
+    const updatedCards = [...cards];
+    updatedCards.splice(index, 1);
+    setCards(updatedCards);
   };
 
   const handleCreateDeck = () => {
     const deck = {
-      cards: cards,
+      cards: cards.map((card) => ({
+        front: card.front,
+        back: {
+          definition: card.definition,
+          synonyms: card.synonyms,
+          example: card.example,
+        },
+      })),
     };
-    console.log("Bộ thẻ đã được tạo:", deck);
+    console.log("Bộ thẻ đã được tạo:", deck.cards);
     // Tiến hành lưu trữ bộ thẻ
     // ...
   };
 
   return (
-    <div className="p-8 height-wrapper">
+    <div className="p-8 height-wrapper ">
       <div className="flex justify-between">
         <div className="text-2xl font-bold">
           <span className="ml-2">Tạo bộ thẻ</span>
         </div>
       </div>
       <div>
+        <div className="my-4">
+          <label htmlFor="folder-select">Chọn thư mục:</label>
+          {/* <Select
+            id="folder-select"
+            defaultValue="default"
+            value={selectedFolder}
+            onChange={setSelectedFolder}
+            className="w-full"
+          >
+            {decks.map((deck) => (
+              <Option key={deck.id} value={deck.id}>
+                {deck.name}
+              </Option>
+            ))}
+          </Select> */}
+        </div>
+        {/* <div className="my-4">
+          <label htmlFor="deck-name-input">Tên bộ thẻ:</label>
+          <Input
+            id="deck-name-input"
+            value={frontCard}
+            onChange={(e) => setFrontCard(e.target.value)}
+          />
+        </div> */}
         <div className="my-4">
           {cards.map((card, index) => (
             <div key={index} className="flex gap-6">
@@ -91,12 +110,12 @@ const CreateDeck = () => {
                   <div className="flex gap-6">
                     <Input
                       id={`definition-input-${index}`}
-                      value={card.back.definition}
+                      value={card.definition}
                       onChange={(e) =>
                         handleCardChange(index, "definition", e.target.value)
                       }
                     />
-                    <Button onClick={() => handleCardChange(index, "definition", card.back.definition)}>Gửi</Button>
+                    <Button>Gửi</Button>
                   </div>
                 </div>
                 <div>
@@ -106,12 +125,12 @@ const CreateDeck = () => {
                   <div className="flex gap-6">
                     <Input
                       id={`synonyms-input-${index}`}
-                      value={card.back.synonyms}
+                      value={card.synonyms}
                       onChange={(e) =>
                         handleCardChange(index, "synonyms", e.target.value)
                       }
                     />
-                    <Button onClick={() => handleCardChange(index, "synonyms", card.back.synonyms)}>Gửi</Button>
+                    <Button>Gửi</Button>
                   </div>
                 </div>
                 <div>
@@ -119,12 +138,12 @@ const CreateDeck = () => {
                   <div className="flex gap-6">
                     <Input
                       id={`example-input-${index}`}
-                      value={card.back.example}
+                      value={card.example}
                       onChange={(e) =>
                         handleCardChange(index, "example", e.target.value)
                       }
                     />
-                    <Button onClick={() => handleCardChange(index, "example", card.back.example)}>Gửi</Button>
+                    <Button>Gửi</Button>
                   </div>
                 </div>
               </div>
