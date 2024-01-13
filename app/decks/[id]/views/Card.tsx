4fe803678933fe7ff41d5a231d9bd7373a5e60a1
LeTruongLam <104148284+LeTruongLam@users.noreply.github.com>
2024-01-14 01:26:27 +0700

    fomat cards

--- app/decks/[id]/views/Card.tsx
+++ app/decks/[id]/views/Card.tsx
@@ -1,9 +1,9 @@
 "use client";
+"use client";
 import React, { useState, useEffect } from "react";
 import { motion } from "framer-motion";
 import "./views.css";
-import { renderToString } from "react-dom/server";
-import Item from "antd/es/list/Item";
+
 const Card = ({ deck_id }) => {
   const token =
     typeof window !== "undefined" ? localStorage.getItem("token") : null;
@@ -47,9 +47,13 @@ const Card = ({ deck_id }) => {
     if (!isAnimating) {
       setIsAnimating(true);
       setIsFlipped(false);
-      setCurrentCardIndex((prevIndex) =>
-        prevIndex === 0 ? cardData.length - 1 : prevIndex - 1
-      );
+      setCurrentCardIndex((prevIndex) => {
+        if (prevIndex === 0) {
+          return cardData.length - 1;
+        } else {
+          return prevIndex - 1;
+        }
+      });
     }
   };
 
@@ -57,13 +61,24 @@ const Card = ({ deck_id }) => {
     if (!isAnimating) {
       setIsAnimating(true);
       setIsFlipped(false);
-      setCurrentCardIndex((prevIndex) => (prevIndex + 1) % cardData.length);
+      setCurrentCardIndex((prevIndex) => {
+        if (prevIndex === cardData.length - 1) {
+          return 0;
+        } else {
+          return prevIndex + 1;
+        }
+      });
     }
   };
 
+  useEffect(() => {
+    setIsFlipped(false);
+    setIsAnimating(false);
+  }, [currentCardIndex]);
+
   return (
     <div>
-      <div className="flex items-center justify-center h-[500px] cursor-pointer	">
+      <div className="mt-6 flex items-center justify-center h-[500px] cursor-pointer	">
         <div
           className="flip-card w-[800px] h-[450px] rounded-md "
           onClick={handleFlip}
@@ -75,24 +90,46 @@ const Card = ({ deck_id }) => {
             transition={{ duration: 0.6 }}
             onAnimationComplete={() => setIsAnimating(false)}
           >
-            {isFlipped ? (
-              <div className="flex items-center justify-center flip-card-back w-[100%] h-[100%] bg-cover text-black p-4 shadow-md hover:shadow-xl rounded-2xl border border-slate-200">
-                <h1>{cardData[currentCardIndex]?.front}</h1>
-              </div>
-            ) : (
-              <div className="flex items-center justify-center flip-card-front w-[100%] h-[100%] bg-cover text-black shadow-md hover:shadow-xl rounded-2xl border border-slate-200 p-4">
-                <h1
-                  dangerouslySetInnerHTML={{
-                    __html: cardData[currentCardIndex]?.back,
-                  }}
-                ></h1>
-              </div>
+            {cardData.length > 0 && (
+              <>
+                {isFlipped ? (
+                  <div className=" flip-card-back w-[100%] h-[100%] bg-cover text-black p-4 shadow-md hover:shadow-xl rounded-2xl border border-slate-200">
+                    <h1 className="text-3xl  flex  w-[100%] h-[100%] flexCenter">
+                      {cardData[currentCardIndex]?.front}
+                    </h1>
+                  </div>
+                ) : (
+                  <div className=" flip-card-front w-[100%] h-[100%] bg-cover text-black shadow-md hover:shadow-xl rounded-2xl border border-slate-200 p-4">
+                   <h1 className="flex flex-col text-3xl w-[100%] h-[100%] flexCenter">
+                                {cardData[currentCardIndex]?.back && (
+                <>
+                  <p>Định nghĩa: {cardData[currentCardIndex]?.back.split("</p><p>")[0].replace(/<p>/g, '')}</p>
+                  <p>Trái nghĩa: {cardData[currentCardIndex]?.back.split("</p><p>")[1].replace(/<p>/g, '')}</p>
+                  <p>Ví dụ: {cardData[currentCardIndex]?.back.split("</p><p>")[2].replace(/<\/p>/g, '')}</p>
+                </>
+              )}
+              </h1>
+                  </div>
+                )}
+              </>
             )}
           </motion.div>
         </div>
       </div>
-      <button onClick={handlePrevious}>Quay lại</button>
-      <button onClick={handleNext}>Tiếp theo</button>
+      <div className="flex justify-center mt-4">
+        <button
+          className="px-4 py-2 mr-2 text-white bg-lime-700 rounded hover:bg-lime-600"
+          onClick={handlePrevious}
+        >
+          Quay lại
+        </button>
+        <button
+          className="px-4 py-2 text-white bg-lime-700 rounded hover:bg-lime-600"
+          onClick={handleNext}
+        >
+          Tiếp theo
+        </button>
+      </div>
     </div>
   );
 };
