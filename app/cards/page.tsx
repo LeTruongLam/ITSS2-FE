fa439d2dbcc23585ed024d0476812df8f93f8e3c
LeTruongLam <104148284+LeTruongLam@users.noreply.github.com>
2024-06-14 15:01:33 +0700

    FE: type

--- app/cards/page.tsx
+++ app/cards/page.tsx
@@ -5,7 +5,7 @@ import "./views.css";
 import { useRouter } from "next/navigation";
 import axios from "axios";
 import useSWR from "swr";
-const fetcher = (url) => axios.get(url).then((res) => res.data);
+const fetcher = (url: string) => axios.get(url).then((res) => res.data);
 
 const ViewCards = () => {
   const router = useRouter();
